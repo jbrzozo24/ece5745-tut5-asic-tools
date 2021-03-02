6b367a3f28e7cc9a3cbce25aff8ce95d72718d94
cb535 <cb535@cornell.edu>
2021-03-02 11:13:42 -0500

    staff update from: ece5745-tut5-asic-tools-0.0-1037-g5870837-dirty [ci skip]

--- sim/tut3_pymtl/sort/__init__.py
+++ sim/tut3_pymtl/sort/__init__.py
@@ -2,7 +2,7 @@
 # sort
 #=========================================================================
 
-# from .SortUnitCL        import SortUnitCL
+from .SortUnitCL        import SortUnitCL
 from .SortUnitFlatRTL   import SortUnitFlatRTL
 from .SortUnitStructRTL import SortUnitStructRTL
 
